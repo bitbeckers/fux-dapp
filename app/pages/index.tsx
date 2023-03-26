977a2c49bad03416f886cc335b0711bf371d06ec
bitbeckers <code@bitbeckers.com>
2023-03-26 13:02:23 +0200

    fix(landing): landing page styling and cta button

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,33 +1,103 @@
-import { Box, Button, Flex, VStack, Text, Link } from "@chakra-ui/react";
+import { UserDocument } from "../.graphclient";
+import ConnectWallet from "../components/ConnectWallet";
+import {
+  Box,
+  Button,
+  Flex,
+  VStack,
+  Text,
+  Link,
+  Spinner,
+  Spacer,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
 import NextLink from "next/link";
+import { useQuery } from "urql";
+import { useAccount } from "wagmi";
 
 const Home: NextPage = () => {
+  const { address } = useAccount();
+  const [result] = useQuery({
+    query: UserDocument,
+    variables: {
+      address: address?.toLowerCase() || "",
+    },
+  });
+
+  const { data, fetching, error } = result;
+
+  const claimLink = (
+    <Flex direction={"column"} gap={2}>
+      <Text fontWeight="500">Claim FUX to get started.</Text>
+      <NextLink href="/start" passHref>
+        <Button p={"1em"} maxW={"xs"}>
+          <Link>Claim FUX</Link>
+        </Button>
+      </NextLink>
+    </Flex>
+  );
+
+  const workstreamLink = (
+    <Flex direction={"column"} gap={2}>
+      <Text fontWeight="500">View your workstreams.</Text>
+      <NextLink href="/workstreams" passHref>
+        <Button p={"1em"} maxW={"xs"}>
+          <Link>Workstreams</Link>
+        </Button>
+      </NextLink>
+    </Flex>
+  );
+
+  const connectWalllet = (
+    <Flex direction={"column"} gap={2}>
+      <Text fontWeight="500">Let&apos;s get connected.</Text>
+      <ConnectWallet />
+    </Flex>
+  );
+
   return (
-    <Flex direction={['column', null, 'row']} mx="auto" maxW="1200px" p={[6, null, 12]}>
-      <Box minW={'50%'}>
-        <Text fontSize={['2xl', null, '3xl']} fontWeight="900">
+    <Flex
+      direction={["column", null, "row"]}
+      mx="auto"
+      maxW="1200px"
+      p={[6, null, 12]}
+    >
+      <Flex direction={"column"} minW={"50%"}>
+        <Text fontSize={["2xl", null, "3xl"]} fontWeight="900">
           How many FUX do you give?
         </Text>
-        <Text fontSize={['lg', null, 'xl']} my={3}>Gain perspective on how to allocate your attention based on perceived value created, as evaluated by your peers.</Text>
-        {/* { account && fuxBalance && ( */}
-          <>
-            <Text mt={12} fontWeight="500">Claim FUX to get started.</Text>
-            <NextLink href="/start" passHref>
-              <Button mt={6} p={"1em"}>
-                <Link>Claim FUX</Link>
-              </Button>
-            </NextLink>
-          </>
-        {/* )} */}
-      </Box>
-      <Box minW={'50%'} pl={[0, null, 12]} pt={[12, null, 0]}>
+        <Text fontSize={["lg", null, "xl"]} my={3}>
+          Gain perspective on how to allocate your attention based on perceived
+          value created, as evaluated by your peers.
+        </Text>
+        <Spacer />
+        {fetching ? (
+          <Spinner size="md" />
+        ) : address ? (
+          data?.user?.fuxer ? (
+            workstreamLink
+          ) : (
+            claimLink
+          )
+        ) : (
+          connectWalllet
+        )}
+      </Flex>
+      <Box minW={"50%"} pl={[0, null, 12]} pt={[12, null, 0]}>
         <VStack gap={3} align="left">
-        <Text textAlign="left" my={3} fontWeight="900">The FUX flywheel</Text>
-        <Text>1. CREATE Workstream(s) and invite contributors</Text>
-        <Text>2. GIVE FUX: Contributors stake their attention into workstreams.</Text>
-        <Text>3. GET vFUX: Peer to Peer evaluations of perceived value created.</Text>
-        <Text><i>... Repeat</i></Text>
+          <Text textAlign="left" my={3} fontWeight="900">
+            The FUX flywheel
+          </Text>
+          <Text>1. CREATE Workstream(s) and invite contributors</Text>
+          <Text>
+            2. GIVE FUX: Contributors stake their attention into workstreams.
+          </Text>
+          <Text>
+            3. GET vFUX: Peer to Peer evaluations of perceived value created.
+          </Text>
+          <Text>
+            <i>... Repeat</i>
+          </Text>
         </VStack>
       </Box>
     </Flex>
