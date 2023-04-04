7caac6bf3fbac4cd3d57987bf7b00dfde42a0f1c
bitbeckers <code@bitbeckers.com>
2023-04-04 20:04:31 +0200

    fix(nav): fix routing back breaking

--- app/pages/finalize/[workstreamID].tsx
+++ app/pages/finalize/[workstreamID].tsx
@@ -16,7 +16,6 @@ import { ethers } from "ethers";
 import { DateTime } from "luxon";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { useQuery } from "urql";
 
 const Resolve: NextPage = () => {
@@ -34,15 +33,6 @@ const Resolve: NextPage = () => {
 
   const { data, fetching, error } = result;
   const _workstream = data?.workstream;
-
-  useEffect(() => {
-    window.onpopstate = () => {
-      router.push({
-        pathname: "/workstream/[workstreamID]",
-        query: { workstreamID },
-      });
-    };
-  });
 
   return _workstream ? (
     <>
