bf63d5e0316f58aeee60de825f0be7ee47d3d12a
bitbeckers <code@bitbeckers.com>
2023-04-04 15:30:16 +0200

    fix(sort): sort workstreams

--- app/pages/workstreams.tsx
+++ app/pages/workstreams.tsx
@@ -6,7 +6,20 @@ import {
 import FuxOverview from "../components/FUX/FuxOverview";
 import WorkstreamModal from "../components/FUX/WorkstreamModal";
 import { WorkstreamRow } from "../components/FUX/WorkstreamRow";
-import { VStack, Divider, Grid, GridItem, Spinner } from "@chakra-ui/react";
+import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
+import {
+  VStack,
+  Divider,
+  Grid,
+  GridItem,
+  Spinner,
+  useBoolean,
+  Button,
+  IconButton,
+  Heading,
+  Center,
+} from "@chakra-ui/react";
+import { BigNumber } from "ethers";
 import type { NextPage } from "next";
 import React from "react";
 import { useQuery } from "urql";
@@ -14,6 +27,9 @@ import { useAccount } from "wagmi";
 
 const Workstreams: NextPage = () => {
   const { address: user } = useAccount();
+  const [sortFux, setSortFux] = useBoolean();
+  const [sortTitle, setSortTitle] = useBoolean();
+  const [sortFunding, setSortFunding] = useBoolean();
 
   const [result, reexecuteQuery] = useQuery({
     query: WorkstreamsByUserDocument,
@@ -22,7 +38,7 @@ const Workstreams: NextPage = () => {
     },
   });
 
-  const { data, fetching, error } = result;
+  const { data: workstreamsByUser, fetching, error } = result;
 
   const [fuxBalanceResponse, reexecuteBalanceQuery] = useQuery({
     query: TokenBalanceDocument,
@@ -39,6 +55,46 @@ const Workstreams: NextPage = () => {
   } = fuxBalanceResponse;
 
   const balance = fuxAvailableData?.tokenBalances.find((balance) => balance);
+
+  const sortedData = workstreamsByUser?.workstreamContributors.sort((a, b) => {
+    if (sortFux) {
+      let fuxGivenA = a.workstream.contributors?.find(
+        (contributor) =>
+          contributor.contributor.id.toLowerCase() === user?.toLowerCase()
+      )?.commitment;
+      let fuxGivenB = b.workstream.contributors?.find(
+        (contributor) =>
+          contributor.contributor.id.toLowerCase() === user?.toLowerCase()
+      )?.commitment;
+
+      console.log("FUX GIVEN A: ", fuxGivenA);
+      console.log("FUX GIVEN B: ", fuxGivenB);
+
+      if (!fuxGivenA) {
+        fuxGivenA = 0;
+      }
+      if (!fuxGivenB) {
+        fuxGivenB = 0;
+      }
+
+      return fuxGivenA < fuxGivenB ? -1 : 1;
+    } else if (sortTitle) {
+      return (a?.workstream?.name || "") < (b?.workstream?.name || "") ? -1 : 1;
+    } else if (sortFunding) {
+      let fundingA = BigNumber.from(a.workstream.funding);
+      let fundingB = BigNumber.from(b.workstream.funding);
+
+      if (!fundingA) {
+        fundingA = BigNumber.from("0");
+      }
+      if (!fundingB) {
+        fundingB = BigNumber.from("0");
+      }
+
+      return fundingA.lt(fundingB) ? -1 : 1;
+    }
+    return 0;
+  });
 
   return (
     <VStack spacing={8} w={"100%"}>
@@ -65,10 +121,72 @@ const Workstreams: NextPage = () => {
             letterSpacing={"0.1em"}
             fontSize="sm"
           >
-            <GridItem colSpan={7}>Title</GridItem>
-            <GridItem colSpan={4}>Funding</GridItem>
-            <GridItem colSpan={2}>FUX</GridItem>
-            <GridItem colSpan={3}>Actions</GridItem>
+            <GridItem colSpan={7}>
+              <Button
+                onClick={() => {
+                  setSortTitle.toggle();
+                  setSortFunding.off();
+                  setSortFux.off();
+                }}
+                variant="ghost"
+                fontSize={"sm"}
+                fontWeight={"normal"}
+                textTransform={"uppercase"}
+                letterSpacing={"0.1em"}
+              >
+                Title
+                <IconButton
+                  aria-label="Sort by FUX"
+                  variant="ghost"
+                  icon={sortTitle ? <ChevronUpIcon /> : <ChevronDownIcon />}
+                />
+              </Button>
+            </GridItem>
+            <GridItem colSpan={4}>
+              <Button
+                onClick={() => {
+                  setSortFunding.toggle();
+                  setSortFux.off();
+                  setSortTitle.off();
+                }}
+                variant="ghost"
+                fontSize={"sm"}
+                fontWeight={"normal"}
+                letterSpacing={"0.1em"}
+                textTransform={"uppercase"}
+              >
+                Funding
+                <IconButton
+                  aria-label="Sort by FUX"
+                  variant="ghost"
+                  icon={sortFunding ? <ChevronUpIcon /> : <ChevronDownIcon />}
+                />
+              </Button>
+            </GridItem>
+            <GridItem colSpan={2}>
+              <Button
+                onClick={() => {
+                  setSortFux.toggle();
+                  setSortFunding.off();
+                  setSortTitle.off();
+                }}
+                variant="ghost"
+                fontSize={"sm"}
+                fontWeight={"normal"}
+                letterSpacing={"0.1em"}
+                textTransform={"uppercase"}
+              >
+                FUX
+                <IconButton
+                  aria-label="Sort by FUX"
+                  variant="ghost"
+                  icon={sortFux ? <ChevronUpIcon /> : <ChevronDownIcon />}
+                />
+              </Button>
+            </GridItem>
+            <GridItem colSpan={2} p="0.5em" textAlign={"center"}>
+              Actions
+            </GridItem>
           </Grid>
           <Grid
             maxW={"800px"}
@@ -77,8 +195,8 @@ const Workstreams: NextPage = () => {
             gap={2}
             templateColumns="repeat(16, 1fr)"
           >
-            {data?.workstreamContributors
-              ? data?.workstreamContributors.map(({ workstream }) =>
+            {sortedData
+              ? sortedData?.map(({ workstream }) =>
                   workstream.status === "Closed" ? undefined : (
                     <WorkstreamRow
                       workstream={workstream as Partial<Workstream>}
