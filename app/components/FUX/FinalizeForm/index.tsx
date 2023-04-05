f21d457a4aef37c6f73573f34a923c78876ffb33
bitbeckers <code@bitbeckers.com>
2023-04-05 14:47:11 +0200

    fix(round): rounding error in finalize

--- app/components/FUX/FinalizeForm/index.tsx
+++ app/components/FUX/FinalizeForm/index.tsx
@@ -33,6 +33,26 @@ const parseEvaluations = (workstream: Workstream) => {
   ) as Ratings;
 };
 
+const roundToTarget = (ratings: Ratings, target: number) => {
+  let l = Object.values(ratings) as number[];
+  let off = target - _.reduce(l, (acc, x) => acc + Math.round(x), 0);
+  let flat = Object.entries(ratings);
+
+  let rounded = _.chain(flat)
+    .sortBy((flat) => Math.round(Number(flat[1])) - Number(flat[1]))
+    .map((x, i) => {
+      let newX = x;
+      newX[1] =
+        Math.round(Number(x[1])) +
+        (off > i ? 1 : 0) -
+        (i >= l.length + off ? 1 : 0);
+      return newX;
+    })
+    .value();
+
+  return Object.fromEntries(rounded) as Ratings;
+};
+
 const calculateRelative = (data: Ratings) => {
   const total = _.sum(Object.values(data));
 
@@ -41,16 +61,7 @@ const calculateRelative = (data: Ratings) => {
     (value) => _.divide(Number(value), Number(total)) * 100
   );
 
-  let sum = 0;
-  Object.entries(relative).forEach(([key, value], i) => {
-    if ((i = Object.keys(relative).length - 1)) {
-      relative[key] = 100 - sum;
-    }
-    relative[key] = Math.round(value);
-    sum = sum + relative[i];
-  });
-
-  return relative;
+  return roundToTarget(relative, 100);
 };
 
 const FinalizeForm: React.FC<{
