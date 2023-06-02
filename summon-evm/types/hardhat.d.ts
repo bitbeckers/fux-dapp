3bcc3f1687c1a791cc4434f175d09d1fd1f4287e
bitbeckers <code@bitbeckers.com>
2023-06-03 00:01:15 +0200

    fix(sc): tweaks, types and compile

--- summon-evm/types/hardhat.d.ts
+++ summon-evm/types/hardhat.d.ts
@@ -81,6 +81,10 @@ declare module "hardhat/types/runtime" {
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.IERC165Upgradeable__factory>;
     getContractFactory(
+      name: "IERC20",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.IERC20__factory>;
+    getContractFactory(
       name: "FUX",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.FUX__factory>;
@@ -170,6 +174,11 @@ declare module "hardhat/types/runtime" {
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.IERC165Upgradeable>;
+    getContractAt(
+      name: "IERC20",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.IERC20>;
     getContractAt(
       name: "FUX",
       address: string,
